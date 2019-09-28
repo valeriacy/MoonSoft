996121072f475684e28b9b2f6a20389278135ec5
JpRod96 <jprodriguez60@gmail.com>
2019-09-28 13:17:02 -0400

    Agregado bat para instalar las dependencias de la automatizacion

---
+++ Calidad/automation.bat
@@ -0,0 +1,17 @@
+choco install -y ruby
+refreshenv
+choco install -y ruby2.devkit
+refreshenv
+gem install cucumber
+gem install capybara
+gem install selenium-webdriver
+gem install selenium
+gem install rspec
+gem install capybara-screenshot
+gem install sinatra
+refreshenv
+choco install -y selenium-chrome-driver
+refreshenv
+choco install -y ansicon
+refreshenv
+pause

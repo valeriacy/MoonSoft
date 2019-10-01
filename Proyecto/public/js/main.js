8b0ca16ca0717044ebdcfdf59b87e4dbe369e9b0
valeriacy <valecyuricev@gmail.com>
2019-09-30 20:46:04 -0400

    Agregado soporte de registro de usuario en el frontend

--- Proyecto/public/js/main.js
+++ Proyecto/public/js/main.js
@@ -13,10 +13,16 @@ app.config(function($routeProvider) {
         templateUrl :"/bienvenido.html",
         controller :"welcomeCtrl"
     })
+    .when("/registro",{
+        templateUrl :"/registro.html",
+        controller :"registroCtrl"
+    })
 });
 
 app.controller("publicCtrl", publicCtrl);
 app.controller("loginCtrl", loginCtrl);
+app.controller("welcomeCtrl", welcomeCtrl);
+app.controller("registroCtrl", registroCtrl);
 
 
 function publicCtrl($scope, $location, $http){
@@ -55,4 +61,49 @@ function loginCtrl($scope, $location, $http){
                     }
                 );
     };
+}
+
+function welcomeCtrl($scope, $location, $http){
+
+}
+
+function registroCtrl($scope, $location, $http){
+    $scope.registrar = () => {
+        nombreCompleto = document.querySelector("#nombreAp").value;
+        direccion = document.querySelector("#direccion").value;
+        telefono = document.querySelector("#telefono").value;
+        profesion = document.querySelector("#profesion").value;
+        email = document.querySelector("#email").value;
+        contrasenha = document.querySelector("#pwd").value;
+
+        nuevo_usuario = {
+            nombre_completo : nombreCompleto,
+            direccion : direccion,
+            telefono : telefono,
+            profesion : profesion,
+            correo : email,
+            contrasenha : contrasenha
+        }
+        
+        let req = {
+            method: 'POST',
+            url: "/api/Usuario",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data : JSON.stringify(nuevo_usuario)
+        };
+
+        consumirApi($http, 
+            req, 
+            (response)=>{
+                if(response.status == 200){
+                    alert("Nuevo usuario registrado")
+                }
+            },
+            (error)=>{
+                console.error(error)
+            }
+        );
+    } 
 }
